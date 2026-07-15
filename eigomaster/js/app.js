532d3a5fa8d6fc54ca82715f01707060235ce9af
swallowsaba <swallowsaba@gmail.com>
2026-07-16 08:07:03 +0900

    Add files via upload

--- eigomaster/js/app.js
+++ eigomaster/js/app.js
@@ -1431,7 +1431,7 @@
   }
 
   /* ---------- Service Worker ---------- */
-  var APP_VERSION = "v99";
+  var APP_VERSION = "v100";
   function registerServiceWorker() {
     if (!("serviceWorker" in navigator)) return;
     if (location.protocol !== "http:" && location.protocol !== "https:") return;
